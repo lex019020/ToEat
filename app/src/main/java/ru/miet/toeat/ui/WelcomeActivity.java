871730d6de5fff277561e82c2af0c8ee4cf6565a
Алексей Лысов <lexazombike72@gmail.com>
2020-11-17 14:19:19 +0300

    implemented main activity

---
+++ app/src/main/java/ru/miet/toeat/ui/WelcomeActivity.java
@@ -0,0 +1,53 @@
+package ru.miet.toeat.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavAction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import ru.miet.toeat.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){
+            //
+        }
+
+        if(tryToLoadData())
+            (new Handler()).postDelayed(this::switchToMainMenu, 500);
+        else
+            (new Handler()).postDelayed(this::switchToSetupPage, 500);
+    }
+
+    private boolean tryToLoadData(){
+        //TODO
+        return true;
+    }
+
+    private void switchToMainMenu(){
+        Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+        intent.setFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY);
+        startActivity(intent);
+    }
+
+    private void switchToSetupPage(){
+        //TODO
+    }
+
+}
