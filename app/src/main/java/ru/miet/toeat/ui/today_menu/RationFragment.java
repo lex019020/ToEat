d1f44c9b7fcfd8f9e01a5087b5023f4238461cb8
Алексей Лысов <lexazombike72@gmail.com>
2020-11-18 13:37:28 +0300

    implemented RationFragment layout

--- app/src/main/java/ru/miet/toeat/ui/today_menu/RationFragment.java
+++ app/src/main/java/ru/miet/toeat/ui/today_menu/RationFragment.java
@@ -2,65 +2,75 @@ package ru.miet.toeat.ui.today_menu;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import ru.miet.toeat.R;
+import ru.miet.toeat.model.Meal;
+import ru.miet.toeat.model.Menu;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class RationFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public RationFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RationFragment newInstance(String param1, String param2) {
-        RationFragment fragment = new RationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private TextView tv_breakfast;
+    private TextView tv_tiffin;
+    private TextView tv_dinner;
+    private TextView tv_aft_snack;
+    private TextView tv_supper;
+    private TextView tv_snack;
+
+    private Layout lay_list;
+    private Layout lay_breakfast;
+    private Layout lay_tiffin;
+    private Layout lay_dinner;
+    private Layout lay_aft_snack;
+    private Layout lay_supper;
+    private Layout lay_snack;
+
+    private Menu today_menu;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ration, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tv_breakfast = getView().findViewById(R.id.tv_breakfast_name);
+        tv_tiffin = getView().findViewById(R.id.tv_btiffin_name);
+        tv_dinner = getView().findViewById(R.id.tv_dinner_name);
+        tv_aft_snack = getView().findViewById(R.id.tv_afternoon_snack_name);
+        tv_supper = getView().findViewById(R.id.tv_supper_name);
+        tv_snack = getView().findViewById(R.id.tv_snack_name);
+
+        //lay_list
+        // TODO
+    }
+
+    private void loadTodayMenu(){
+        // TODO
     }
 }
